3cc4c637226ea4f752f9f77c3126801a0a0c1b86
YoonsungNam <ysnam@dcslab.snu.ac.kr>
2022-06-05 14:30:48 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by mini shiba on 2022/06/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
